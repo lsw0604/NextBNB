952c121890e21335162c24a76468f94e92fa9150
lsw0604 <57swji@naver.com>
2022-07-06 18:10:19 +0900

    common Selector

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { ReactNodeArray, useState } from "react";
 import styled from "styled-components";
 import CloseXIcon from "../../public/static/svg/modal/modal_close_x_icon.svg";
 import MailIcon from "../../public/static/svg/auth/mail.svg";
 import PersonIcon from "../../public/static/svg/auth/person.svg";
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
 import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
-import Input from "../common/input";
+import Input from "../common/Input";
+import { monthList, dayList, yearList } from "../../lib/staticData";
+import palette from "../../styles/palette";
+import Selector from "../common/Selector";
 
 const Container = styled.div`
   width: 568px;
@@ -30,6 +33,34 @@ const Container = styled.div`
       cursor: pointer;
     }
   }
+
+  .sign-up-birthday-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-modal-birthday-month-selector {
+      margin-right: 16px;
+      flex-grow: 1;
+    }
+    .sign-up-modal-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-year-selector {
+      width: 33.3333%;
+    }
+  }
 `;
 
 const SignUpModal: React.FC = () => {
@@ -38,22 +69,34 @@ const SignUpModal: React.FC = () => {
   const [firstName, setFirstName] = useState("");
   const [password, setPassword] = useState("");
   const [hidePassword, setHidePassword] = useState(true);
+  const [birthYear, setBirthYear] = useState<string | undefined>();
+  const [birthMonth, setBirthMonth] = useState<string | undefined>();
+  const [birthDay, setBirthDay] = useState<string | undefined>();
 
   const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
-  }
+  };
   const onChangeLastName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLastName(event.target.value);
-  }
+  };
   const onChangeFirstName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFirstName(event.target.value);
-  }
+  };
   const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
-  }
+  };
   const toggleHidePassword = () => {
     setHidePassword(!hidePassword);
-  }
+  };
+  const onChangeBirthYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthYear(event.target.value);
+  };
+  const onChangeBirthMonth = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthMonth(event.target.value);
+  };
+  const onChangeBirthDay = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthDay(event.target.value);
+  };
 
   return (
     <Container>
@@ -98,6 +141,40 @@ const SignUpModal: React.FC = () => {
           value={password}
           onChange={onChangePassword}
         />
+      </div>
+      <p className="sign-up-birthday-label">Birth Day</p>
+      <p className="sign-up-modal-birthday-info">
+        Only adults 18 years of age or older can join as a member.<br />
+        Birthday is not open to other AirBNB users.
+      </p>
+      <div className="sign-up-modal-birthday-selectors">
+        <div className="sign-up-modal-birthday-month-selector">
+          <Selector 
+            options={monthList}
+            disabledOptions={["Month"]}
+            defaultValue="Month"
+            value={birthMonth}
+            onChange={onChangeBirthMonth}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-day-selector">
+          <Selector 
+            options={dayList}
+            disabledOptions={["Day"]}
+            defaultValue="Day"
+            value={birthDay}
+            onChange={onChangeBirthDay}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-year-selector">
+          <Selector 
+            options={yearList}
+            disabledOptions={["Year"]}
+            defaultValue="Year"
+            value={birthYear}
+            onChange={onChangeBirthYear}
+          />
+        </div>
       </div>
     </Container>
   ) 
---
+++ lib/staticData.ts
@@ -0,0 +1,18 @@
+export const monthList = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const dayList = Array.from(Array(31), (_, i) => String(i + 1));
+
+export const yearList = Array.from(Array(123), (_, i) => String(2022 - i));
