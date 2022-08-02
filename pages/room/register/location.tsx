108aef63f3d8150cbaa169acc830d5390ed9a3e0
lsw0604 <57swji@gmail.com>
2022-08-02 17:56:48 +0900

    location init

---
+++ pages/room/register/location.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import RegisterLocation from "../../../components/register/room/RegisterRoomLocation";
+
+const location: NextPage = () => {
+  return <RegisterLocation />;
+};
+
+export default location;
