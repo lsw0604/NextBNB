c1e45333afdf8f02e54c377b030785019148ab3e
lsw0604 <57swji@gmail.com>
2022-08-18 17:40:39 +0900

    RegisterAmentities Init

---
+++ pages/room/register/amentities.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import RegisterRoomAmentities from "../../../components/register/room/RegisterRoomAmentities";
+
+const amentities: NextPage = () => {
+  return <RegisterRoomAmentities />;
+};
+
+export default amentities;
